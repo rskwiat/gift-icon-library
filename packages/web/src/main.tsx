6d61844ed6e8162d4d1ae817cf4e8d85b91a99f2
Robert Skwiat <rob@robertskwiat.com>
2025-03-06 10:19:44 -0500

    inital commit

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+import App from './App.tsx';
+
+const root = document.getElementById('root') as HTMLElement;
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
